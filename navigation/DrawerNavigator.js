f84df9fcb9992d166e5f56c2ae5dc52be2963ec0
Marcos Monteiro da Cruz <162529045+mmonteirin@users.noreply.github.com>
2026-04-21 09:02:42 -0300

    Atualização - Navegação

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,20 +1,29 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import TabNavigator from "./TabNavigator";
-
-import PerfilMenu from "../screens/PerfilMenu";
-import PerfilDeclararOcorrencia from "../screens/PerfilDeclararOcorrencia";
-import EventoDetalhes from "../screens/EventoDetalhes";
+import CustomDrawerContent from "./CustomDrawerNavigator";
+import TelaFeed from "../screens/TelaFeed";
+import PerfilOcorrencia from "../screens/perfilOcorrencia";
+import perfilMenu from "../screens/perfilMenu";
+import Suporte from "../screens/TelaSuporte";
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
-	return (
-		<Drawer.Navigator>
-			<Drawer.Screen name="Home" component={TabNavigator} />
-			<Drawer.Screen name="Menu" component={PerfilMenu} />
-			<Drawer.Screen name="Ocorrencia" component={PerfilDeclararOcorrencia} />
-			<Drawer.Screen name="Evento" component={EventoDetalhes} />
-		</Drawer.Navigator>
-	);
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerStyle: { backgroundColor: "#6200ee" },
+        headerTintColor: "#fff",
+        drawerActiveTintColor: "#6200ee",
+      }}
+    >
+      <Drawer.Screen name="Tela Inicial" component={TabNavigator} />
+      <Drawer.Screen name="Feed" component={TelaFeed} />
+      <Drawer.Screen name="Suas Avaliações" component={PerfilOcorrencia} />
+      <Drawer.Screen name="Perfil" component={perfilMenu} />
+      <Drawer.Screen name="Suporte Técnico" component={Suporte} />
+    </Drawer.Navigator>
+  );
 }
